f52b9b9bcc50582ffa299b8ed0e2385c3c27bae9
Khang Phan <khang.phan@opswat.com>
2022-09-09 17:54:06 +0700

    add serve file

--- 06-files/image.go
+++ 06-files/image.go
@@ -26,9 +26,32 @@ func dogPig(w http.ResponseWriter, r *http.Request) {
 	io.Copy(w, f)
 }
 
+func serveDog(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "corgi.jpg")
+}
+
+func serveContent(w http.ResponseWriter, r *http.Request) {
+	f, err := os.Open("corgi.jpg")
+	if err != nil {
+		http.Error(w, "file not found", 404)
+		return
+	}
+	defer f.Close()
+
+	fi, err := f.Stat()
+	if err != nil {
+		http.Error(w, "file not found", 404)
+		return
+	}
+
+	http.ServeContent(w, r, fi.Name(), fi.ModTime(), f)
+}
+
 func (imageServer) executeMain() {
 	http.Handle("/", http.HandlerFunc(d))
 	http.Handle("/dog", http.HandlerFunc(dogPig))
+	http.Handle("/serve-dog", http.HandlerFunc(serveDog))
+	http.Handle("/serve-content", http.HandlerFunc(serveContent))
 	err := http.ListenAndServe(":8089", nil)
 	if err != nil {
 		log.Println("Can not start server on port 8089")
